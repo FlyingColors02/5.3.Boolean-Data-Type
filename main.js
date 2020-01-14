490182907be388774f9ff183ee7167beb2eed357
FlyingColors02 <shraduborse97@gmail.com>
2020-01-14 11:09:14 +0530

    #shraddha borse: boolean data type

---
+++ main.js
@@ -0,0 +1,16 @@
+/*     DATA TYPE 
+    1. Value Type    --> NUMBER, STRING, BOOLEAN
+    2.Reference Type --> ARRAY, CLASS              */
+
+//Boolean Data Type:
+
+let boolValue1; //declaration
+ boolValue1=true;  //defining boolean value
+ console.log(boolValue1); //true
+ console.log(typeof(boolValue1)); //boolean
+
+
+ boolValue1=false;  //defining boolean value
+ console.log(boolValue1); //false
+ console.log(typeof(boolValue1)); //boolean
+
